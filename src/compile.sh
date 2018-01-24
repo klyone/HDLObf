54197874568a960994647a8bd41ee1b112030a20
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2018-01-24 10:16:57 +0100

    src/compile.sh: Fix bin folder creation issue.
    Signed-off-by: Miguel Jimenez Lopez <mjl.klyone@gmail.com>

--- src/compile.sh
+++ src/compile.sh
@@ -4,7 +4,11 @@ VERILOG_GRAMMAR="Verilog2001.g4"
 VHDL_GRAMMAR="vhdl.g4"
 
 source ./source_classpath.sh
-mkdir ../bin
+
+if [ ! -d "../bin" ] ; then
+	mkdir ../bin
+fi
+
 (java org.antlr.v4.Tool -o ./generated ${VERILOG_GRAMMAR} > ../log/SystemVerilogLexer_OutputLog.dat)  >& ../log/SystemVerilogLexer_ErrorLog.dat
 (java org.antlr.v4.Tool -o ./generated ${VHDL_GRAMMAR} > ../log/VHDLLexer_OutputLog.dat)  >& ../log/VHDLLexer_ErrorLog.dat
 (javac -d ../bin  ./generated/*.java *.java ) >& ../log/JavacErrorLog.dat
